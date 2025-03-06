e09fc9e5cd76327b5c89fc7a4424c85508b79d97
Jess Wojciechowski <jess@pumas.ai>
2025-03-06 14:17:44 -0500

    Re-arranged to follow previous layout, added background info on AOG and CairoMakie

--- Day1/01-TeachingMaterial/05-data_read_wrangle/02-data_visualization.jl
+++ Day1/01-TeachingMaterial/05-data_read_wrangle/02-data_visualization.jl
@@ -1,12 +1,32 @@
-# Script: 02-data_visualization.jl
-# Purpose: Visualize warfarin PK/PD data
-# ========================================================
+# =============================================================================
+# Data Wrangling and Visualization - Part 2: Visualizing Data
+# =============================================================================
 
-using AlgebraOfGraphics, CairoMakie
+# The recommended Julia packages for performing data visualization and generating
+# publication-quality figures are:
+# - AlgebraOfGraphics.jl: https://aog.makie.org/stable/
+# - CairoMakie.jl: https://docs.makie.org/stable/explanations/backends/cairomakie
 
+# AlgebraOfGraphics provides a set of tools for plotting data in Julia. Its 
+# design and functionality are similar to theat of ggplot2 in R, whereby it 
+# involves the development of layers (data, mapping aesthetics, and geometrics)
+# to build a plot.
+
+# CairoMakie is the underlying plotting system for AlgebraOfGraphics.jl using a
+# Cairo backend to draw vector graphics to SVG and PNG.
+
+# Import the previous data reading example for demonstration
 include("01-data_reading.jl")
 
-@info "A first plot of DV against TIME"
+# -----------------------------------------------------------------------------
+# 1. PACKAGES FOR VISUALIZING DATA
+# -----------------------------------------------------------------------------
+using AlgebraOfGraphics, CairoMakie
+
+# -----------------------------------------------------------------------------
+# 2. PLOT DV VERSUS TIME
+# -----------------------------------------------------------------------------
+# A first plot of DV against TIME"
 data(
     dropmissing(df, "DV")
 ) * mapping(
@@ -14,7 +34,13 @@ data(
     "DV",
 ) * visual(Lines) |> draw
 
-@info "...what a mess! There are two endpoints. We need to split the plot on the DVID variable"
+#...what a mess! There are two endpoints. We need to split the plot on the DVID
+# variable
+
+# -----------------------------------------------------------------------------
+# 3. STRATIFY BY ENDPOINT
+# -----------------------------------------------------------------------------
+
 data(
     dropmissing(df, "DV")
 ) * mapping(
@@ -23,7 +49,12 @@ data(
     row = "DVID" => nonnumeric # to interpret it as discrete 
 ) * visual(Lines) |> draw
 
-@info "...the graphs wrap around. We also need to group on ID"
+# ...the graphs wrap around. We also need to group on ID
+
+# -----------------------------------------------------------------------------
+# 4. GROUP BY ID
+# -----------------------------------------------------------------------------
+
 data(
     dropmissing(df, "DV")
 ) * mapping(
@@ -33,7 +64,13 @@ data(
     group = "ID" => nonnumeric
 ) * visual(Lines) |> draw
 
-@info "...better but the two endpoints have different scale to we need to loosen the yaxes"
+# ...better but the two endpoints have different scale to we need to loosen the
+# y-axes
+
+# -----------------------------------------------------------------------------
+# 5. SEPARATE Y-AXIS FOR EACH ENDPOINT
+# -----------------------------------------------------------------------------
+
 data(
     dropmissing(df, "DV")
 ) * mapping(
@@ -46,7 +83,12 @@ data(
     facet = (;linkyaxes = :minimal)
 )
 
-@info "...many lines are overlapping so let us adjust the opacity"
+# ...many lines are overlapping so let us adjust the opacity
+
+# -----------------------------------------------------------------------------
+# 6. ADJUST OPACITY OF LINES
+# -----------------------------------------------------------------------------
+
 data(
     dropmissing(df, "DV")
 ) * mapping(
@@ -62,7 +104,12 @@ data(
     facet = (;linkyaxes = :minimal)
 )
 
-@info "...almost there. We need a title and some better looking labels"
+# ...almost there. We need a title and some better looking labels
+
+# -----------------------------------------------------------------------------
+# 7. FIGURE AND AXIS LABELS
+# -----------------------------------------------------------------------------
+
 data(
     dropmissing(df, "DV")
 ) * mapping(
@@ -76,10 +123,13 @@ data(
 ) |> f -> draw(
     f;
     facet = (;linkyaxes = :minimal),
-    figure = (; title = "Warfarin data"),
+    figure = (;title = "Warfarin data"),
 )
 
-@info "Covariate effects: SEX"
+# -----------------------------------------------------------------------------
+# 8. COLOR BY SEX
+# -----------------------------------------------------------------------------
+
 data(
     dropmissing(df, "DV")
 ) * mapping(
@@ -93,10 +143,13 @@ data(
 ) |> f -> draw(
     f;
     facet = (;linkyaxes = :minimal),
-    figure = (; title = "Warfarin data"),
+    figure = (;title = "Warfarin data"),
 )
 
-@info "Covariate effects: AGE"
+# -----------------------------------------------------------------------------
+# 9. COLOR BY AGE
+# -----------------------------------------------------------------------------
+
 data(
     dropmissing(df, "DV")
 ) * mapping(
@@ -110,10 +163,13 @@ data(
 ) |> f -> draw(
     f;
     facet = (;linkyaxes = :minimal),
-    figure = (; title = "Warfarin data"),
+    figure = (;title = "Warfarin data"),
 )
 
-@info "Covariate effects: AGE"
+# -----------------------------------------------------------------------------
+# 10. COLOR BY AMOUNT
+# -----------------------------------------------------------------------------
+
 data(
     dropmissing(df, "DV")
 ) * mapping(
@@ -127,5 +183,20 @@ data(
 ) |> f -> draw(
     f;
     facet = (;linkyaxes = :minimal),
-    figure = (; title = "Warfarin data"),
+    figure = (;title = "Warfarin data"),
 )
+
+# -----------------------------------------------------------------------------
+# TIPS AND BEST PRACTICES
+# -----------------------------------------------------------------------------
+# 1. General structure of plotting layers consist of data, mapping and visual
+# elements
+# 2. Elements of a layer are concatenated using * (multiplication operator)
+# 3. Multiple layers are superimposed onto each other using + (addition operator)
+# 4. The implementation is consistent with the order of operations, therefore
+# the order of which layers are sequentially added is important
+# 5. Once all layers are combined, the resulting plot is compiled using draw
+
+# Next Steps:
+# 1. Data wrangling (03-data_wrangling.jl)"
+# 2. Creating a Pumas population object
