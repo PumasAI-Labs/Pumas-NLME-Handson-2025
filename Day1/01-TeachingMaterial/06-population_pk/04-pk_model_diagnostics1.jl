1a44d5e8bde46bbffb0538bc76943e5a2af113a8
Mélanie Wilbaux <melanie@pumas.ai>
2025-04-02 12:57:42 +0000

    All PK modeling moved to Day1

--- Day1/01-TeachingMaterial/06-population_pk/04-pk_model_diagnostics1.jl
+++ Day1/01-TeachingMaterial/06-population_pk/04-pk_model_diagnostics1.jl
@@ -22,7 +22,7 @@ include("03-pk_model_fitting.jl")  # This gives us the fitted model, warfarin_pk
 # 1. PACKAGES FOR GOODNESS-OF-FIT DIAGNOSTICS
 # -----------------------------------------------------------------------------
 using Pumas
-using CairoMakie
+using CairoMakie,  AlgebraOfGraphics
 using DataFrames, DataFramesMeta
 using CategoricalArrays
 using Logging
@@ -78,6 +78,54 @@ fig_id_conc = subject_fits(warfarin_pkmodel_pred,
 # We can render all plots in one go by calling display on the vector
 display.(fig_id_conc)
 
+
+# -----------------------------------------------------------------------------
+# 2. NORMALIZED PREDICTION DISTRIBUTION ERRORS (NPDE)
+# -----------------------------------------------------------------------------
+# Obtain individual- and population-predictions from the model fit
+warfarin_pkmodel_pred = inspect(warfarin_pkmodel_fit, nsim = 1000)
+warfarin_pkmodel_preddf = DataFrame(warfarin_pkmodel_pred)
+
+# Generating goodness-of-fit diagnostics when NPDEs are available will now
+# plot them
+goodness_of_fit(warfarin_pkmodel_pred)
+
+# Custom plots can be generated using the DataFrame output of inspect and
+# using AlgebraOfGraphics.jl/CairoMakie.jl packages
+p_npde_scatter = data(warfarin_pkmodel_preddf)*
+    mapping(:time,:conc_npde)*
+    visual(Scatter)
+draw(p_npde_scatter)
+
+# A linear regression line can be added
+p_npde_linear = data(dropmissing(warfarin_pkmodel_preddf,:conc))*
+    mapping(:time,:conc_npde)*
+    AlgebraOfGraphics.linear()*
+    visual(;label = "Linear Regression")
+draw(p_npde_scatter + p_npde_linear)
+
+# And a LOESS smooth line can be added
+p_npde_loess = data(dropmissing(warfarin_pkmodel_preddf,:conc))*
+    mapping(:time,:conc_npde)*
+    AlgebraOfGraphics.smooth()*
+    visual(;color = :red,label = "LOESS")
+draw(p_npde_scatter + p_npde_linear + p_npde_loess)
+
+# Adjust figure options
+draw(p_npde_scatter + p_npde_linear + p_npde_loess;
+    axis = (;
+        xlabel = "Time After Dose (hours)",
+        ylabel = "NPDE",
+        xticks = LinearTicks(7),
+        yticks = [-6,-2,0,2,6],
+    ),
+    legend = (;
+        position = :bottom,
+        framevisible = false
+    )
+)
+
+
 # -----------------------------------------------------------------------------
 # 4. VISUAL PREDICTIVE CHECK
 # -----------------------------------------------------------------------------
@@ -117,6 +165,93 @@ fig_vpc = vpc_plot(
 )
 # Save plot
 save(joinpath(@__DIR__, "vpc_concentration.png"), fig_vpc)
+
+
+# -----------------------------------------------------------------------------
+# 3. ADDITIONAL VPC OPTIONS
+# -----------------------------------------------------------------------------
+# More on VPCs
+# Pumas uses local quantile regression instead of binning. The approach is inspired by
+# https://ascpt.onlinelibrary.wiley.com/doi/full/10.1002/psp4.12319
+
+# Redo default vpc
+vpc_res_conc = vpc(warfarin_pkmodel_fit)
+
+# Generate figure for VPC
+vpc_plot(
+    vpc_res_conc,
+    simquantile_medians = true,
+    observations = true,
+    axis = (
+        xlabel = "Time After Dose (hr)",
+        ylabel = "Warfarin Concentration (mg/L)",
+    ),
+    include_legend = true,
+    figurelegend = (
+        position = :b,
+        framevisible = false,
+        orientation = :vertical, 
+        tellheight = true,
+        tellwidth = false,
+        nbanks = 3
+    ),
+    figure = (size = (800, 600),)
+)
+
+# Adjust the bandwidth for local quantile regression
+# The neightborhood of the local regression is controlled by the bandwidth parameter
+vpc_res_conc_bw10 = vpc(
+    warfarin_pkmodel_fit;
+    bandwidth = 10.0,
+)
+
+vpc_plot(
+    vpc_res_conc_bw10,
+    simquantile_medians = true,
+    observations = true,
+    axis = (
+        xlabel = "Time After Dose (hr)",
+        ylabel = "Warfarin Concentration (mg/L)",
+    ),
+    include_legend = true,
+    figurelegend = (
+        position = :b,
+        framevisible = false,
+        orientation = :vertical, 
+        tellheight = true,
+        tellwidth = false,
+        nbanks = 3
+    ),
+    figure = (size = (800, 600),)
+)
+
+# An alternative way to account for subject heterogeneity is to
+# stratify the plot based on discrete covariates.
+vpc_res_conc_by_sex = vpc(
+    warfarin_pkmodel_fit;
+    stratify_by = [:SEX]
+)
+
+vpc_plot(
+    vpc_res_conc_by_sex,
+    simquantile_medians = true,
+    observations = true,
+    axis = (
+        xlabel = "Time After Dose (hr)",
+        ylabel = "Warfarin Concentration (mg/L)",
+    ),
+    include_legend = true,
+    figurelegend = (
+        position = :b,
+        framevisible = false,
+        orientation = :vertical, 
+        tellheight = true,
+        tellwidth = false,
+        nbanks = 3
+    ),
+    figure = (size = (800, 600),)
+)
+
 
 # -----------------------------------------------------------------------------
 # TIPS AND BEST PRACTICES
