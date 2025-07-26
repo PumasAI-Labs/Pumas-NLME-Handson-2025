4d45bb8a74ca0d1c45766f88affb089015cded96
David Müller-Widmann <devmotion@users.noreply.github.com>
2025-07-26 16:09:42 +0200

    Update PKPD simulation exercises and solutions

--- Exercises/01-pkpd_simulations_exercises.jl
+++ Exercises/01-pkpd_simulations_exercises.jl
@@ -3,7 +3,7 @@
 # =============================================================================
 
 # Import the previous code that returns the fitted Pumas model
-include(joinpath(@__DIR__, "..","01-TeachingMaterial","02-population_pkpd","03-pkpd_model_fitting.jl") ) # to get warfarin_model and warfarin_model_fit    
+include(joinpath(@__DIR__, "..","TeachingMaterial","population_pkpd","03-pkpd_model_fitting.jl")) # to get warfarin_model and warfarin_model_fit    
 
 # -----------------------------------------------------------------------------
 # 1. PACKAGES 
@@ -29,7 +29,7 @@ Using the warfarin PK-PD model:
 2. Explore the effect on PK and PCA
 """
 
-# Create DosagRegimen object for single dose levels of 50 and 150 at time 0
+# Create DosagRegimen objects for single dose levels of 50 and 150 at time 0
 # your code here
 
 # Create a population of 100 subjects for each new dosing regimen:
@@ -54,7 +54,7 @@ Using the warfarin PK-PD model:
 2. Explore the effect on PK and PCA
 """
 
-# Create DosagRegimen object for daily doses of 50 and 150 for a week:
+# Create DosagRegimen objects for daily doses of 50 and 150 for a week:
 #  your code here
 
 # Create a population for each new dosing regimen:
@@ -79,7 +79,7 @@ Using the warfarin PK-PD model:
 2. Explore the effect on PK and PCA
 """
 
-# Create DosagRegimen object for a loading dose of 400 followed by daily doses of 150 for a week:
+# Create a DosagRegimen object for a loading dose of 400 followed by daily doses of 150 for a week:
 #  your code here
 
 # Create a population with the new dosing regimen:
